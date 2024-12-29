08e87d9a4148f76ad6d21e61e0e599745fb537e5
SaraSaber <saraasaber77@gmail.com>
2024-12-29 17:06:15 +0330

    feat(DeterminingGameStarter): select item and open bottomSheet

--- app/src/main/java/ir/developer/goalorpooch_compose/ui/screen/DeterminingGameStarter.kt
+++ app/src/main/java/ir/developer/goalorpooch_compose/ui/screen/DeterminingGameStarter.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -26,8 +25,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -67,10 +64,12 @@ import kotlin.random.Random
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DeterminingGameStarter(navController: NavController) {
-
-    Scaffold(
-        modifier = Modifier.fillMaxSize()
-    ) { innerPadding ->
+    val items = listOf(
+        ItemStarterModel(0, R.drawable.pic_team_one, "تیم اول بازی رو شروع می کند."),
+        ItemStarterModel(1, R.drawable.pic_team_two, "تیم دوم بازی رو شروع می کند."),
+        ItemStarterModel(2, R.drawable.pic_random_box, "انتخاب تصادفی آغازکننده")
+    )
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
             Column(
                 modifier = Modifier
@@ -85,9 +84,9 @@ fun DeterminingGameStarter(navController: NavController) {
 
                 Text(
                     modifier = Modifier.padding(
-                        top = PaddingTop(),
-                        start = PaddingRound(),
                         end = PaddingRound(),
+                        start = PaddingRound(),
+                        top = PaddingTop(),
                         bottom = 30.sdp
                     ),
                     text = "به دو تیم تقسیم شوید و سپس تیم آغاز کننده بازی را مشخص کنید.",
@@ -97,36 +96,8 @@ fun DeterminingGameStarter(navController: NavController) {
                     textAlign = TextAlign.Justify
                 )
 
-                val items = remember { mutableStateListOf<ItemStarterModel>() }
-                items.add(
-                    ItemStarterModel(
-                        id = 0,
-                        image = R.drawable.pic_team_one,
-                        text = "تیم اول بازی رو شروع می کند."
-                    )
-                )
-                items.add(
-                    ItemStarterModel(
-                        id = 1,
-                        image = R.drawable.pic_team_two,
-                        text = "تیم دوم بازی رو شروع می کند."
-                    )
-                )
-                items.add(
-                    ItemStarterModel(
-                        id = 2,
-                        image = R.drawable.pic_random_box,
-                        text = "انتخاب تصادفی آغازکننده"
-                    )
-                )
-                items.forEachIndexed { _, item ->
-                    ItemGameStarter(
-                        image = item.image,
-                        text = item.text,
-                        idItem = item.id,
-                        paddingValues = innerPadding,
-                        navController = navController
-                    )
+                items.forEach { item ->
+                    ItemGameStarter(item, navController)
                 }
             }
         }
@@ -135,187 +106,102 @@ fun DeterminingGameStarter(navController: NavController) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ItemGameStarter(
-    image: Int,
-    idItem: Int,
-    paddingValues: PaddingValues,
-    navController: NavController,
-    text: String
-) {
+fun ItemGameStarter(item: ItemStarterModel, navController: NavController) {
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     var showBottomSheet by remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier
-            .padding(
-                top = PaddingTop(),
-                start = PaddingRound(),
-                end = PaddingRound()
-            )
-            .border(
-                width = 1.sdp,
-                color = Color.White,
-                shape = RoundedCornerShape(8.dp)
-            )
+            .padding(end = PaddingRound(), start = PaddingRound(), top = PaddingTop())
+            .border(1.sdp, Color.White, RoundedCornerShape(8.dp))
             .clip(RoundedCornerShape(8.dp))
             .fillMaxWidth()
-            .background(color = FenceGreen)
-            .clickable(
-                enabled = true,
-                onClick = {
-                    showBottomSheet = true
-                }
-            ),
+            .background(FenceGreen)
+            .clickable { showBottomSheet = true },
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             modifier = Modifier
-                .padding(
-                    top = PaddingTopMedium(),
-                    start = PaddingTopMedium(),
-                    end = PaddingRound(),
-                    bottom = PaddingTopMedium()
-                )
+                .padding(PaddingTopMedium())
                 .size(SizePicMedium()),
-            painter = painterResource(id = image),
-            contentDescription = "pic_team_one",
+            painter = painterResource(id = item.image),
+            contentDescription = null
         )
 
         Text(
-            text = text,
+            text = item.text,
             fontSize = DescriptionSize(),
             fontFamily = FontPeydaMedium,
             color = Color.White,
             textAlign = TextAlign.Justify
         )
-
-        if (showBottomSheet) {
-
-            ModalBottomSheet(
-                modifier = Modifier.padding(paddingValues),
-                onDismissRequest = {
-                    showBottomSheet = false
-                },
-                sheetState = sheetState,
-                shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-                containerColor = FenceGreen,
-                tonalElevation = 16.sdp
-            ) {
-                // Sheet content
-                BottomSheetContent(
-                    image = image,
-                    text = text,
-                    idItem = idItem,
-                    onCardSelection = {
-                        scope.launch {
-                            sheetState.hide()
-                            navController.navigate(Utils.SELECT_CARD_SCREEN)
-                        }.invokeOnCompletion { showBottomSheet = false }
-                    },
-                    onDismiss = {
-                        scope.launch { sheetState.hide() }
-                            .invokeOnCompletion { showBottomSheet = false }
-                    }
-                )
-            }
-        }
     }
 
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState,
+            shape = RoundedCornerShape(16.dp),
+            containerColor = FenceGreen
+        ) {
+            BottomSheetContent(item, onDismiss = {
+                scope.launch { sheetState.hide() }.invokeOnCompletion { showBottomSheet = false }
+            }, onCardSelection = {
+                scope.launch {
+                    sheetState.hide()
+                    navController.navigate(Utils.SELECT_CARD_SCREEN)
+                }.invokeOnCompletion { showBottomSheet = false }
+            })
+        }
+    }
 }
 
 @Composable
 fun BottomSheetContent(
-    image: Int,
-    text: String,
-    idItem: Int,
+    item: ItemStarterModel,
     onDismiss: () -> Unit,
     onCardSelection: () -> Unit
 ) {
+    val randomIndex = if (item.id == 2) Random.nextInt(0, 2) else item.id
+    Utils.STARTER_GAME = randomIndex
+
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        if (idItem == 2) {
-            var randomItem by remember { mutableIntStateOf(-1) }
-            randomItem = Random.nextInt(0, 2)
+        val selectedImage =
+            if (randomIndex == 0) R.drawable.pic_team_one else R.drawable.pic_team_two
+        val selectedText =
+            if (randomIndex == 0) "تیم اول بازی رو شروع می کند." else "تیم دوم بازی رو شروع می کند."
 
-            Utils.STARTER_GAME = randomItem
+        Image(
+            modifier = Modifier
+                .padding(PaddingRound())
+                .size(SizePicLarge()),
+            painter = painterResource(id = selectedImage),
+            contentDescription = null
+        )
 
-            if (randomItem == 0) {
-                Image(
-                    modifier = Modifier
-                        .padding(start = PaddingRound(), end = PaddingRound())
-                        .size(SizePicLarge()),
-                    painter = painterResource(id = R.drawable.pic_team_one),
-                    contentDescription = null
-                )
-                Text(
-                    modifier = Modifier.padding(
-                        top = PaddingTopMedium(),
-                        start = PaddingRound(),
-                        end = PaddingRound()
-                    ),
-                    text = "تیم اول بازی رو شروع می کند.",
-                    fontSize = TitleSize(),
-                    fontFamily = FontPeydaMedium,
-                    color = Color.White,
-                    textAlign = TextAlign.Justify
-                )
-            } else {
-                Image(
-                    modifier = Modifier
-                        .padding(start = PaddingRound(), end = PaddingRound())
-                        .size(SizePicLarge()),
-                    painter = painterResource(id = R.drawable.pic_team_two),
-                    contentDescription = null
-                )
-                Text(
-                    modifier = Modifier.padding(
-                        top = PaddingTopMedium(),
-                        start = PaddingRound(),
-                        end = PaddingRound()
-                    ),
-                    text = "تیم دوم بازی رو شروع می کند.",
-                    fontSize = TitleSize(),
-                    fontFamily = FontPeydaMedium,
-                    color = Color.White,
-                    textAlign = TextAlign.Justify
-                )
-            }
-        } else {
-            Utils.STARTER_GAME = idItem
-            Image(
-                modifier = Modifier
-                    .padding(start = PaddingRound(), end = PaddingRound())
-                    .size(SizePicLarge()),
-                painter = painterResource(id = image),
-                contentDescription = null
-            )
-            Text(
-                modifier = Modifier.padding(
-                    top = PaddingTopMedium(),
-                    start = PaddingRound(),
-                    end = PaddingRound()
-                ),
-                text = text,
-                fontSize = TitleSize(),
-                fontFamily = FontPeydaMedium,
-                color = Color.White,
-                textAlign = TextAlign.Justify
-            )
-        }
+        Text(
+            modifier = Modifier.padding(PaddingTopMedium()),
+            text = selectedText,
+            fontSize = TitleSize(),
+            fontFamily = FontPeydaMedium,
+            color = Color.White,
+            textAlign = TextAlign.Justify
+        )
+
         Row(modifier = Modifier.padding(top = PaddingTopLarge())) {
             Button(
                 modifier = Modifier
-                    .padding(top = PaddingTopMedium(), start = PaddingRound(), end = PaddingRound())
-                    .weight(1f),
+                    .weight(1f)
+                    .padding(PaddingRound()),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = Color.White,
                     contentColor = FenceGreen
                 ),
-                contentPadding = PaddingValues(8.sdp),
                 onClick = onCardSelection
             ) {
                 Text(
@@ -325,12 +211,12 @@ fun BottomSheetContent(
                     color = FenceGreen
                 )
             }
+
             OutlinedButton(
                 modifier = Modifier
-                    .padding(top = PaddingTopMedium(), start = PaddingRound(), end = PaddingRound())
-                    .weight(1f),
+                    .weight(1f)
+                    .padding(PaddingRound()),
                 border = BorderStroke(1.sdp, Color.White),
-                contentPadding = PaddingValues(8.sdp),
                 onClick = onDismiss
             ) {
                 Text(
